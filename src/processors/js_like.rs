19dfbb3d9742fdc77bd4570cee0dc17bed57c059
LiuJie <1547698569@qq.com>
2025-06-16 00:21:38 +0800

    feat: for node path deal

--- src/processors/js_like.rs
+++ src/processors/js_like.rs
@@ -1,6 +1,8 @@
+use crate::common::has_file_extension;
 use glob::glob;
 use std::collections::HashSet;
-use std::fs;
+use std::path::{Path, PathBuf};
+use std::{env, fs};
 use swc_common::errors::{ColorConfig, Handler};
 use swc_common::input::StringInput;
 use swc_common::sync::Lrc;
@@ -64,12 +66,33 @@ fn parse_ts_or_tsx(code: String) -> Module {
     module
 }
 
+fn try_to_find_files_without_a_suffix(relative_path_for_project: String) -> String {
+    let candidates = [
+        format!("{}.ts", relative_path_for_project),
+        format!("{}.tsx", relative_path_for_project),
+        format!("{}/index.ts", relative_path_for_project),
+        format!("{}/index.tsx", relative_path_for_project),
+        format!("{}.d.ts", relative_path_for_project),
+        format!("{}/index.d.ts", relative_path_for_project),
+    ];
+
+    for candidate in &candidates {
+        if Path::new(&candidate).exists() {
+            return candidate.to_string();
+        }
+    }
+    format!(
+        "{}(Unknown file type, the file does not exist in the scan directory, or is not a TSX, TS or .d.ts file)",
+        relative_path_for_project
+    )
+}
+
 pub fn get_js_like_import_info() {
     let mut import_collector = ImportCollector::default();
     for entry in glob("src/**/*.{ts,tsx}").expect("Failed to read glob pattern") {
         match entry {
             Ok(path) => {
-                if (path.is_file()) {
+                if path.is_file() {
                     let code = fs::read_to_string(path).unwrap();
                     let module = parse_ts_or_tsx(code);
                     module.visit_with(&mut import_collector);
@@ -78,6 +101,41 @@ pub fn get_js_like_import_info() {
             Err(e) => println!("{:?}", e),
         }
     }
+}
+
+fn path_to_real_path(current_file_path: String, import_path: String) -> String {
+    if import_path.starts_with("@/") {
+        let relative_path_for_project = import_path.replace("@/", "src/");
+        return if has_file_extension(&relative_path_for_project) {
+            relative_path_for_project
+        } else {
+            try_to_find_files_without_a_suffix(relative_path_for_project)
+        };
+    }
+
+    if import_path.starts_with("..") || import_path.starts_with(".") {
+        let get_path = |base: &str, path: &str| -> String {
+            PathBuf::from(base)
+                .join(path)
+                .canonicalize()
+                .unwrap()
+                .to_str()
+                .unwrap()
+                .to_string()
+        };
+        let absolute_path = get_path(&current_file_path, &import_path);
+        //TODO fix rust中并没有node中的path.relative，需要手动实现
+        let relative_path_for_project = get_path(
+            env::current_dir().unwrap().to_str().unwrap(),
+            &absolute_path,
+        );
+        return if has_file_extension(&relative_path_for_project) {
+            relative_path_for_project
+        } else {
+            try_to_find_files_without_a_suffix(relative_path_for_project)
+        };
+    }
+    import_path
 }
 
 #[cfg(test)]
